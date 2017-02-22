444b5ce74a011ee5643c17fcbd86957232a73c82
Scott White <scott@Scotts-MBP-2.lan>
2017-02-21 23:10:28 -0500

    simple twitter api proxy

---
+++ index.js
@@ -0,0 +1,21 @@
+var config  = require('./config').twitter;
+var Utils = require('./utils')();
+
+exports.handler = function(event, context, callback) {
+  Utils.bearerToken(config.consumer_key, config.consumer_secret).then(function(bt){
+    Utils.tweets('trump').then(function(tweets){
+      console.log('have tweets');
+      var response = {
+        statusCode: 200,
+        headers: {
+          'Content-Type': 'application/json', 
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Headers': 'Content-type'
+        },
+        body: tweets
+      };
+      callback(null, response);
+    })
+  });   
+}
+
---
+++ utils.js
@@ -0,0 +1,76 @@
+var http = require('https');
+
+function Utils(){
+  var url = 'https://api.twitter.com/oauth2/token';
+  var token;
+  function bearerToken(key, secret){
+    return new Promise(function(resolve, reject){
+      var cat = key +":"+secret;
+      var credentials = new Buffer(cat).toString('base64');
+      console.log(credentials);
+      var headers= {
+          "Authorization": "Basic " + credentials,
+          "Content-Type":"application/x-www-form-urlencoded;charset=UTF-8"
+      };
+      var body = "grant_type=client_credentials";
+      var request = http.request({
+        headers: headers,
+        method: 'POST',
+        hostname:'api.twitter.com',
+        path: '/oauth2/token'
+      }, function(response){
+        var str = '';
+        response.on('data', function (chunk) {
+          str += chunk;
+        });
+
+        response.on('end', function () {
+          var tmp = JSON.parse(str);
+          token = tmp.access_token;
+          console.log(token);
+          resolve(token);
+        });
+
+      });
+      request.end(body);
+    });
+  }
+  function tweets(search){
+    return new Promise(function(resolve, reject){
+      var headers = {
+          "Authorization": "Bearer " + token
+      };
+      var request = http.request({
+        headers: headers,
+        method: 'GET',
+        hostname:'api.twitter.com',
+        path: '/1.1/search/tweets.json?q=trump&result_type=mixed&count=20'
+      }, function(response){
+        var str = '';
+        response.on('error',function(e){reject(e)});
+
+        response.on('data', function (chunk) {
+          str += chunk;
+        });
+
+        response.on('end', function () {
+          try{
+            resolve(str);
+          }catch(e){
+            reject(e);
+          }
+          
+        });
+      });
+      request.end();
+    });
+  }
+
+
+  this.bearerToken = bearerToken;
+  this.tweets = tweets;
+  this.token = token;
+  return this;
+}
+
+module.exports = Utils;
